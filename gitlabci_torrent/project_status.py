3e7022a70163d134832a8d838f0414b82cee54ce
jacksonpradolima <pradolima@live.com>
2020-03-31 15:00:58 -0300

    Update project status

--- gitlabci_torrent/project_status.py
+++ gitlabci_torrent/project_status.py
@@ -6,17 +6,34 @@ import os
 class ProjectStatus(object):
     def __init__(self, project_dir):
         self.project_dir = project_dir
+        self.project = project_dir.split(os.sep)[-1]
 
-    def generate_summary(self):
-        summary_cols = ["Period", "Builds", "Faults", "Faulty builds", "Tests", "Test suite size"]
+    def update_project(self, project_dir):
+        self.project_dir = project_dir
+        self.project = project_dir.split(os.sep)[-1]
+
+    def get_summary(self):
+        summary_cols = ["Name", "Period", "Builds", "Faults",
+                        "Faulty builds", "Tests", "Test suite size",
+                        "Duration", "Interval"]
+
         summary = pd.DataFrame(columns=summary_cols)
 
         df = pd.read_csv(f'{self.project_dir}{os.sep}data-filtering.csv', sep=';', thousands=',')
 
-        df.last_run = pd.to_datetime(df.last_run)
+        df['last_run'] = pd.to_datetime(df['last_run'])
 
         # Sort by commits arrival and start
         df = df.sort_values(by=['last_run'])
+
+        # Convert to minutes only valid build duration
+        duration = [x / 60 for x in df.duration.tolist()]
+
+        dates = pd.to_datetime(df['last_run'].unique())
+
+        # Difference between commits arrival - Convert to minutes to improve the view
+        diff_date = [(dates[i] - dates[i + 1]).seconds /
+                     60 for i in range(len(dates) - 1)]
 
         total_builds = df.job_id.nunique()
 
@@ -26,14 +43,13 @@ class ProjectStatus(object):
 
         builds = df['job_id'].max()
 
-        faults = df.query('verdict > 0').groupby(
-            ['job_id'], as_index=False).agg({'verdict': np.sum})['verdict']
+        faults = df.query('verdict > 0').groupby(['job_id'], as_index=False).agg({'verdict': np.sum})
 
         # Number of builds in which at least one test failed
-        faulty_builds = faults.count()
+        faulty_builds = faults['verdict'].count() if len(faults) > 0 else 0
 
         # Total of faults
-        total_faults = faults.sum()
+        total_faults = faults['verdict'].sum() if len(faults) > 0 else 0
 
         # number of unique tests identified from build logs
         test_max = df['test_name'].nunique()
@@ -44,11 +60,12 @@ class ProjectStatus(object):
         test_suite_min = tests['test_name'].min()
         test_suite_max = tests['test_name'].max()
 
-        row = [mindate + "-" + maxdate, total_builds, total_faults, faulty_builds,
-               test_max, str(test_suite_min) + "-" + str(test_suite_max)]
+        row = [self.project.split("@")[-1], mindate + "-" + maxdate, total_builds, total_faults, faulty_builds,
+               test_max, str(test_suite_min) + "-" + str(test_suite_max),
+               f"{round(np.mean(duration), 4)} ({round(np.std(duration), 3)})" if len(duration) > 0 else "-",
+               f"{round(np.mean(diff_date), 4)} ({round(np.std(diff_date), 3)})" if len(diff_date) > 0 else "-"]
 
         summary = summary.append(pd.DataFrame(
             [row], columns=summary_cols), ignore_index=True)
 
-        print("\n")
-        print(summary.head())
+        return summary
--- gitlabci_torrent/test/project_status_test.py
+++ gitlabci_torrent/test/project_status_test.py
@@ -1,15 +1,72 @@
 import os
 import unittest
+import pandas as pd
+from tabulate import tabulate
 
 from gitlabci_torrent.project_status import ProjectStatus
+from gitlabci_torrent.data_extraction import DataExtraction
 
 
 class RunningAnalyzer(unittest.TestCase):
 
-    def test_harvester_split(self):
+    def test_main_system(self):
         a = ProjectStatus(f'logs{os.sep}libssh@libssh-mirror')
 
-        a.generate_summary()
+        print(a.get_summary().head())
+
+        self.assertTrue(True)
+
+    def test_total_set(self):
+        print("\nInformation about total set", end="")
+
+        a = ProjectStatus(f'logs{os.sep}libssh@libssh-mirror{os.sep}libssh@total')
+
+        print(a.get_summary().head())
+
+        self.assertTrue(True)
+
+    def test_variants(self):
+        de = DataExtraction(f'logs{os.sep}libssh@libssh-mirror')
+        variants = de.get_variants()
+
+        summary_cols = ["Name", "Period", "Builds", "Faults",
+                        "Faulty builds", "Tests", "Test suite size"]
+
+        df = pd.DataFrame(columns=summary_cols)
+
+        a = ProjectStatus(f'')
+
+        for variant in variants:
+            variant = variant.replace('/', '-')
+            a.update_project(f'logs{os.sep}libssh@libssh-mirror{os.sep}libssh@{variant}')
+            df = df.append(a.get_summary())
+
+        print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
+
+        self.assertTrue(True)
+
+    def test_both(self):
+        print("\n")
+        de = DataExtraction(f'logs{os.sep}libssh@libssh-mirror')
+        variants = sorted(de.get_variants())
+
+        summary_cols = ["Name", "Period", "Builds", "Faults",
+                        "Faulty builds", "Tests", "Test suite size",
+                        "Duration", "Interval"]
+
+        df = pd.DataFrame(columns=summary_cols)
+
+        a = ProjectStatus(f'logs{os.sep}libssh@libssh-mirror{os.sep}libssh@total')
+
+        df = df.append(a.get_summary())
+
+        for variant in variants:
+            variant = variant.replace('/', '-')
+            a.update_project(f'logs{os.sep}libssh@libssh-mirror{os.sep}libssh@{variant}')
+            df = df.append(a.get_summary())
+
+        print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
+        print(df.to_latex(index=False))
 
         self.assertTrue(True)
 
