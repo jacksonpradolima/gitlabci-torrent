eff6863384064c34cc50605657b62ba0ad1ea21e
jacksonpradolima <pradolima@live.com>
2020-12-09 14:47:29 -0300

    update main analyzer

--- analyzer_main.py
+++ analyzer_main.py
@@ -5,12 +5,12 @@ from gitlabci_torrent.analyzer import Analyzer
 if __name__ == '__main__':
     ap = argparse.ArgumentParser(description='Analyzer - Mining the Logs')
 
-    ap.add_argument('-d', '--save_dir',
+    ap.add_argument('-d', '--logs_dir',
                     help='Directory with the logs. For instance: logs/core@dune-common', default='logs')
 
     args = ap.parse_args()
 
-    a = Analyzer(args.save_dir)
+    a = Analyzer(args.logs_dir)
     a.run()
 
     print("The analyzer process ended!")
