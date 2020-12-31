caabc8695a4a8373ca2a6853fe1646716ab9f794
jacksonpradolima <pradolima@live.com>
2020-12-30 23:50:54 -0300

    updata data extraction

--- data_extraction_main.py
+++ data_extraction_main.py
@@ -4,7 +4,7 @@ from gitlabci_torrent.data_extraction import DataExtraction
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser(
-        description='Data Extraction - Mining the Logs')
+        description='Data Extraction - Splitting by Variants')
 
     ap.add_argument('-d', '--log_dir',
                     help='Directory with the logs. For instance: logs/core@dune-common', default='logs')
@@ -15,10 +15,7 @@ if __name__ == '__main__':
     args = ap.parse_args()
 
     a = DataExtraction(args.log_dir, args.project_name)
-
-    #print("Extrating Total Set...")
     a.extract_total_set()
-
-    #a.extract_by_variant()
+    a.extract_by_variant()
 
     print("The data extraction process ended!")
